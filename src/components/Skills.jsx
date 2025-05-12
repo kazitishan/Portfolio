c81326e375fb72ed7c39c348fa55ea2f9d74cf67
kazitishan <kazitishan12@gmail.com>
2025-05-12 03:33:44 -0400

    Added keys to the skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,18 +1,18 @@
-function Skills(){
+function Skills() {
     const languages = ["Java", "Javascript", "Python", "Racket", "R"];
     const frameworks = ["Spring Boot", "Tailwind CSS", "Express.js", "Next.js"];
     const libraries = ["React", "Matplotlib", "D3.js"];
     const databases = ["PostgreSQL", "MongoDB", "MySQL"];
 
-    return(
+    return (
         <div className="rounded-xl border bg-card text-card-foreground shadow m-4">
             <div className="rounded-t-xl flex justify-center font-semibold p-1">Skills</div>
 
             <div className="skills mb-2 px-3">
                 <div className="flex items-center">
                     <p>Languages:</p>
-                    {[...languages].map((language) => (
-                        <div className="rounded-lg bg-gray-700 m-1 p-1">
+                    {languages.map((language, index) => (
+                        <div key={`language-${index}`} className="rounded-lg bg-gray-700 m-1 p-1">
                             <p className="text-xs font-bold">{language}</p>
                         </div>
                     ))}
@@ -20,8 +20,8 @@ function Skills(){
 
                 <div className="flex items-center">
                     <p>Frameworks:</p>
-                    {[...frameworks].map((framework) => (
-                        <div className="rounded-lg bg-gray-700 m-1 p-1">
+                    {frameworks.map((framework, index) => (
+                        <div key={`framework-${index}`} className="rounded-lg bg-gray-700 m-1 p-1">
                             <p className="text-xs font-bold">{framework}</p>
                         </div>
                     ))}
@@ -29,8 +29,8 @@ function Skills(){
 
                 <div className="flex items-center">
                     <p>Libraries:</p>
-                    {[...libraries].map((library) => (
-                        <div className="rounded-lg bg-gray-700 m-1 p-1">
+                    {libraries.map((library, index) => (
+                        <div key={`library-${index}`} className="rounded-lg bg-gray-700 m-1 p-1">
                             <p className="text-xs font-bold">{library}</p>
                         </div>
                     ))}
@@ -38,15 +38,13 @@ function Skills(){
 
                 <div className="flex items-center">
                     <p>Databases:</p>
-                    {[...databases].map((database) => (
-                        <div className="rounded-lg bg-gray-700 m-1 p-1">
+                    {databases.map((database, index) => (
+                        <div key={`database-${index}`} className="rounded-lg bg-gray-700 m-1 p-1">
                             <p className="text-xs font-bold">{database}</p>
                         </div>
                     ))}
                 </div>
             </div>
-
-
         </div>
     );
 }
