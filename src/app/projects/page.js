61c1476637b4a798271fd73d3f07632abba40ffe
kazitishan <kazitishan12@gmail.com>
2025-05-15 01:48:58 -0400

    Added projects to the projects page

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,8 +1,44 @@
+import ProjectCard from "@/components/ProjectCard";
 
+const projects = [
+  {
+    img: "/prisoners.png",
+    title: "100 Prisoners Simulation",
+    description: "This is a tool that allows users to simulate the unique solution to the 100 prisoners puzzle",
+    technologies: [
+      "JavaScript",
+      "D3.js"
+    ],
+    website: "https://kazitishan.github.io/100-Prisoners-Sim/",
+    source: "https://github.com/kazitishan/100-Prisoners-Sim"
+  },
+  {
+    img: "/wordle.png",
+    title: "Wordle Solver Tool",
+    description: "An interactive Wordle solver that suggests optimal guesses using letter frequency data and provides real-time data visualizations.",
+    technologies: [
+      "Python",
+      "Matplotlib"
+    ],
+    source: "https://github.com/kazitishan/Wordle-Solver"
+  }
+]
+
+export const metadata = {
+  title: "Kazi Tishan | Projects",
+  description: "My projects",
+};
 
 export default function Projects() {
   return (
     <main className="container">
+      <div className="flex justify-center font-semibold p-1">My Projects</div>
+      
+      <div className="flex">
+        {[...projects].reverse().map((project, i) => (
+          <ProjectCard data={project} key={i} />
+        ))}
+      </div>
       
     </main>
   );
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,8 +1,40 @@
-function ProjectCard({project}){
-    return(
-        <div className="p-2 flex flex-col rounded-xl border bg-card text-card-foreground shadow m-4">
-            <div className="w-xs h-xs">
-                <img src={project.img} alt={project.name} className="rounded-xl"/>
+function ProjectCard({data}) {
+    return (
+        <div className="p-3 flex flex-col rounded-xl border bg-card text-card-foreground shadow m-4 w-sm justify-between">
+            <img src={data.img} alt={data.title} className="rounded-xl w-auto h-auto"/>
+
+            <h3 className="font-bold">{data.title}</h3>
+
+            <div className="text-sm">
+                {data.description}
+            </div>
+
+            <div className="flex flex-wrap gap-1 mt-1 justify-center">
+                {data.technologies.map((tech, i) => (
+                    <div key={i} className="rounded-md bg-gray-700 p-0.5">
+                        <p className="text-[10px] font-bold">{tech}</p>
+                    </div>
+                ))}
+            </div>
+
+            <div className="flex gap-2 mt-2 justify-center">
+                {data.website && (
+                    <a href={`${data.website}`} target="_blank" rel="noopener noreferrer">
+                        <div className="flex align-middle p-2 bg-white rounded-lg hover:bg-gray-300 transition cursor-pointer items-center">
+                            <p className="text-xs font-semibold text-black">Website</p>
+                            <img src="/link.svg" alt="Link" className="w-4 h-4 ml-1 filter invert" />
+                        </div>
+                    </a>
+                )}
+
+                {data.source && (
+                    <a href={`${data.source}`} target="_blank" rel="noopener noreferrer">
+                        <div className="flex align-middle p-2 bg-white rounded-lg hover:bg-gray-300 transition cursor-pointer items-center">
+                            <p className="text-xs font-semibold text-black">Source</p>
+                            <img src="/github.svg" alt="Link" className="w-4 h-4 ml-1 filter invert" />
+                        </div>
+                    </a>
+                )}
             </div>
         </div>
     );
