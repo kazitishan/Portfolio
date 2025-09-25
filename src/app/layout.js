2e275e55f99498682367a6e24561afe7558da200
kazitishan <kazitishan12@gmail.com>
2025-09-25 14:58:32 -0400

    Adding vercel web analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from '@/components/Footer';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ['latin'],
